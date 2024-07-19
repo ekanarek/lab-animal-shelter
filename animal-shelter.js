ce82d72f062fc7d908e0dca0bf5281e67ce8da8d
Emily Kanarek <emilyakanarek@gmail.com>
2024-07-19 17:27:20 -0400

    Finish lab

--- animal-shelter.js
+++ animal-shelter.js
@@ -18,6 +18,38 @@ class Animal {
     }
 }
 
+class Cat extends Animal {
+    constructor(name, color, hunger = 50) {
+        super(name, 'cat', color, hunger);
+        this.food = 'fish';
+    }
+
+    greet() {
+        console.log(`Meow, I'm ${this.name} the ${this.species}`);
+    }
+
+    feed() {
+        this.hunger -= 20;
+        console.log(`Yum, I love ${this.food}`);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, color, hunger = 50) {
+        super(name, 'dog', color, hunger);
+        this.food = 'kibble';
+    }
+
+    greet() {
+        console.log(`Woof, I'm ${this.name} the ${this.species}`);
+    }
+
+    feed() {
+        this.hunger -= 20;
+        console.log(`Yum, I love ${this.food}`);
+    }
+}
+
 class AnimalShelter {
     constructor() {
         this.animals = [];
@@ -28,7 +60,31 @@ class AnimalShelter {
     }
 
     adopt(animal) {
-        const animalIndex = this.animals.indedOf(animal);
+        const animalIndex = this.animals.indexOf(animal);
         this.animals.splice(animalIndex, 1);
     }
+
+    getAnimalsBySpecies(species) {
+        return this.animals.filter(a => a.species === species);
+    }
+}
+
+const shelter = new AnimalShelter();
+
+for (const a of animalData) {
+    let animal;
+    const hunger = a.hunger ? a.hunger : 50;
+    if (a.species === 'dog') {
+        animal = new Dog(a.name, a.color, hunger);
+    } else if (a.species === 'cat') {
+        animal = new Cat(a.name, a.color, hunger);
+    } else {
+        animal = new Animal(a.name, a.species, a.color, hunger);
+    }
+    shelter.addAnimal(animal);
+}
+
+for (const animal of shelter.animals) {
+    animal.greet();
+    animal.feed();
 }
