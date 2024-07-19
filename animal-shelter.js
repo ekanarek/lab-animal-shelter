0a5570b504f3774a146d94651abdf5dc8f1896d4
Emily Kanarek <emilyakanarek@gmail.com>
2024-07-19 16:56:33 -0400

    Create classes

--- animal-shelter.js
+++ animal-shelter.js
@@ -1,1 +1,34 @@
 const animalData = require('./animal-data.json');
+
+class Animal {
+    constructor(name, species, color, hunger = 50) {
+        this.name = name;
+        this.species = species;
+        this.color = color;
+        this.hunger = hunger;
+    }
+
+    greet() {
+       console.log(`Hi I'm ${this.name} the ${this.species}!`); 
+    }
+
+    feed() {
+        this.hunger -= 20;
+        console.log('Yum, I love food.');
+    }
+}
+
+class AnimalShelter {
+    constructor() {
+        this.animals = [];
+    }
+
+    addAnimal(animal) {
+        this.animals.push(animal);
+    }
+
+    adopt(animal) {
+        const animalIndex = this.animals.indedOf(animal);
+        this.animals.splice(animalIndex, 1);
+    }
+}
